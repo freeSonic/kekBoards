677775d3ae004691e383ff940ef101419c914fa2
freeSonic <77183052+freeSonic@users.noreply.github.com>
2021-01-09 17:53:28 -0800

    basic login page

---
+++ chatNode.js
@@ -0,0 +1,12 @@
+
+function sleep(milliseconds) {
+  const date = Date.now();
+  let currentDate = null;
+  do {
+    currentDate = Date.now();
+  } while (currentDate - date < milliseconds);
+}
+onmessage = function(e){
+	sleep(400);
+	postMessage('connected');
+}
