ec45a24ca1db54979d4caa1ee1ce5befafbb038a
freeSonic <77183052+freeSonic@users.noreply.github.com>
2021-01-10 13:59:35 -0800

    Update cryptico.js

--- cryptico.js
+++ cryptico.js
@@ -2,14 +2,9 @@
 // All Rights Reserved.
 // See "LICENSE" for details.
 // Basic JavaScript BN library - subset useful for RSA encryption.
-if (typeof exports !== 'undefined') {
-  if (typeof module !== 'undefined' && module.exports) {
-    exports = module.exports = _;
-  }
-  exports._ = _;
-} else {
-  root['_'] = _;
-}
+try {
+   module.exports = exports = MyModule;
+} catch (e) {console.log(e);}
 // Bits per digit
 var dbits;
 
